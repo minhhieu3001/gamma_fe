d90b82fa808887a3ba67dba162cec464bb3b172d
leanhduc123 <leeanhducc@gmail.com>
2022-03-30 22:26:20 +0700

    fix

---
+++ static/js/bundle.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (function() { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (function() {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nError: [BABEL] D:\\\\Project\\\\gamma_fe\\\\src\\\\index.js: Using `babel-preset-react-app` requires that you specify `NODE_ENV` or `BABEL_ENV` environment variables. Valid values are \\\"development\\\", \\\"test\\\", and \\\"production\\\". Instead, received: undefined. (While processing: \\\"D:\\\\\\\\Project\\\\\\\\gamma_fe\\\\\\\\node_modules\\\\\\\\babel-preset-react-app\\\\\\\\index.js\\\")\\n    at module.exports (D:\\\\Project\\\\gamma_fe\\\\node_modules\\\\babel-preset-react-app\\\\create.js:58:11)\\n    at module.exports (D:\\\\Project\\\\gamma_fe\\\\node_modules\\\\babel-preset-react-app\\\\index.js:19:10)\\n    at async (D:\\\\Project\\\\gamma_fe\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\gensync-utils\\\\async.js:44:33)\\n    at async (D:\\\\Project\\\\gamma_fe\\\\node_modules\\\\gensync\\\\index.js:186:15)\\n    at D:\\\\Project\\\\gamma_fe\\\\node_modules\\\\gensync\\\\index.js:216:13\\n    at Generator.next (<anonymous>)\\n    at D:\\\\Project\\\\gamma_fe\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\full.js:229:21\\n    at Generator.next (<anonymous>)\\n    at Function.<anonymous> (D:\\\\Project\\\\gamma_fe\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\gensync-utils\\\\async.js:25:3)\\n    at Generator.next (<anonymous>)\");\n\n//# sourceURL=webpack://gamma_app/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
